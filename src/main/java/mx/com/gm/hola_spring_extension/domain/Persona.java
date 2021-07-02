c6769e1bd4b154b32f1bce501d5f82c9da009b91
MKluna <matiaslunastyles@gmail.com>
2021-07-02 19:06:19 -0300

    Mejoras Visuales

--- src/main/java/mx/com/gm/hola_spring_extension/domain/Persona.java
+++ src/main/java/mx/com/gm/hola_spring_extension/domain/Persona.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -30,4 +31,7 @@ public class Persona implements Serializable {
   private String email;
   
   private String telefono;
+
+  @NotNull
+  private Double saldo;
 }
